b1972be5c6370c3a94fce72bd591264d9cedf44f
prasmalla <malla.prasanna@gmail.com>
2014-06-26 18:50:44 -0400

    spree update

--- app/models/spree/taxon_map.rb
+++ app/models/spree/taxon_map.rb
@@ -2,6 +2,6 @@ module Spree
   class TaxonMap < ActiveRecord::Base
     belongs_to :taxons
     
-    attr_accessible :product_type, :taxon_id, :priority
+#     attr_accessible :product_type, :taxon_id, :priority
   end
 end
